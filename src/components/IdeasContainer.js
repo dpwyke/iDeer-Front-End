fbaf63e69045d7c7e36d2fb4326f713c2a3bfdf2
glehr058 <dpwyke@gmail.com>
2019-02-18 18:50:41 -0500

    Initial commit

---
+++ src/components/IdeasContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class IdeasContainer extends Component {
+  render() {
+    return (
+      <div>
+        Ideas
+      </div>
+    )
+  }
+}
+
+export default IdeasContainer
